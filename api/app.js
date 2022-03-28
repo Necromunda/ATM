bb91385efe24a6cdacbf1c710b355abaa821c3e4
Johannes Rantapää <johannes.rantapaa@gmail.com>
2022-03-28 15:54:15 +0300

    users crud operaatioiden luonnostelua

--- api/app.js
+++ api/app.js
@@ -3,18 +3,18 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/users_route');
 
 var app = express();
 
+app.use(helmet());
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 module.exports = app;
--- api/database.js
+++ api/database.js
@@ -1,5 +1,13 @@
 const mysql = require('mysql');
 const dotenv = require('dotenv');
+
 dotenv.config();
-const connection = mysql.createPool(process.env.SQL_SERVER);
+
+const connection = mysql.createPool({
+    host: sql11.freemysqlhosting.net,
+    user: sql11482083,
+    password: nHMRfbCNTC,
+    database: sql11482083
+});
+
 module.exports = connection;
