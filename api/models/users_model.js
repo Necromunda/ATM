ef3b87887668e08b08265341df46b828e78980c3
Johannes Rantapää <johannes.rantapaa@gmail.com>
2022-03-29 15:45:59 +0300

    säätöä

--- api/models/users_model.js
+++ api/models/users_model.js
@@ -1,16 +1,16 @@
 const db = require('../dbconnection');
 
 const users = {
+  add: function(users, callback) {
+    return db.query(
+      'insert into users (fname,lname,address,phone) values(?,?,?,?)',
+      [users.fname, users.lname, users.address, users.phone], callback);
+  },
   getById: function(id, callback) {
     return db.query('select * from users where user_id=?', [id], callback);
   },
   getAll: function(callback) {
     return db.query('select * from users', callback);
-  },
-  add: function(users, callback) {
-    return db.query(
-      'insert into users (fname,lname,address,phone) values(?,?,?,?)',
-      [users.fname, users.lname, users.address, users.phone], callback);
   },
   update: function(id, users, callback) {
     return db.query(
