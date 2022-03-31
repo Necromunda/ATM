6fd4f037681ecf1549ec81f65e72bc3ede4f3376
Johannes Rantapää <johannes.rantapaa@gmail.com>
2022-03-31 10:57:07 +0300

    Siivousta

--- api/dbconnection.js
+++ api/dbconnection.js
@@ -6,7 +6,7 @@ dotenv.config();
 const dbconnection = mysql.createPool({
     host:'127.0.0.1',
     user: 'root',
-    password: 'root',
+    password: 'testi',
     database: 'banksimul'
 })
 
--- bankautomat/RFID/rfid.h
+++ bankautomat/RFID/rfid.h
@@ -2,11 +2,13 @@
 #define RFID_H
 
 #include "RFID_global.h"
+#include <QDebug>
 
 class RFID_EXPORT RFID
 {
 public:
     RFID();
+    ~RFID();
 };
 
 #endif // RFID_H
