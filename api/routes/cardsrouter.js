32ccc6ec070bbb647f26db6f6528bc2b8a4e3dd1
Johannes Rantapää <johannes.rantapaa@gmail.com>
2022-04-06 11:51:51 +0300

    Muutettu accounts-, cards-, ja usersrouter palauttamaan json-objekti arrayn sijasta getById:llä

--- api/routes/cardsrouter.js
+++ api/routes/cardsrouter.js
@@ -20,7 +20,7 @@ router.get('/:id?',
       if (err) {
         response.json(err);
       } else {
-        response.json(dbResult);
+        response.json(dbResult[0]);
       }
     });
   } else {
