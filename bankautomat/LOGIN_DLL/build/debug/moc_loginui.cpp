fb54e7acde5bde1bbacb2cdaf945928fd86b96bc
Johannes Rantapää <johannes.rantapaa@gmail.com>
2022-04-26 09:52:18 +0300

    ?

---
+++ bankautomat/LOGIN_DLL/build/debug/moc_loginui.cpp
@@ -0,0 +1,237 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'loginui.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.15.2)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <memory>
+#include "../../loginui.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'loginui.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.15.2. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_LoginUi_t {
+    QByteArrayData data[23];
+    char stringdata0[403];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_LoginUi_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_LoginUi_t qt_meta_stringdata_LoginUi = {
+    {
+QT_MOC_LITERAL(0, 0, 7), // "LoginUi"
+QT_MOC_LITERAL(1, 8, 15), // "sendPinToEngine"
+QT_MOC_LITERAL(2, 24, 0), // ""
+QT_MOC_LITERAL(3, 25, 11), // "aboutToQuit"
+QT_MOC_LITERAL(4, 37, 10), // "closeEvent"
+QT_MOC_LITERAL(5, 48, 12), // "QCloseEvent*"
+QT_MOC_LITERAL(6, 61, 10), // "startTimer"
+QT_MOC_LITERAL(7, 72, 9), // "stopTimer"
+QT_MOC_LITERAL(8, 82, 7), // "timeout"
+QT_MOC_LITERAL(9, 90, 10), // "resetTimer"
+QT_MOC_LITERAL(10, 101, 8), // "wrongPin"
+QT_MOC_LITERAL(11, 110, 24), // "on_pushButton_Ok_clicked"
+QT_MOC_LITERAL(12, 135, 27), // "on_pushButton_Clear_clicked"
+QT_MOC_LITERAL(13, 163, 23), // "on_pushButton_0_clicked"
+QT_MOC_LITERAL(14, 187, 23), // "on_pushButton_1_clicked"
+QT_MOC_LITERAL(15, 211, 23), // "on_pushButton_2_clicked"
+QT_MOC_LITERAL(16, 235, 23), // "on_pushButton_3_clicked"
+QT_MOC_LITERAL(17, 259, 23), // "on_pushButton_4_clicked"
+QT_MOC_LITERAL(18, 283, 23), // "on_pushButton_5_clicked"
+QT_MOC_LITERAL(19, 307, 23), // "on_pushButton_6_clicked"
+QT_MOC_LITERAL(20, 331, 23), // "on_pushButton_7_clicked"
+QT_MOC_LITERAL(21, 355, 23), // "on_pushButton_8_clicked"
+QT_MOC_LITERAL(22, 379, 23) // "on_pushButton_9_clicked"
+
+    },
+    "LoginUi\0sendPinToEngine\0\0aboutToQuit\0"
+    "closeEvent\0QCloseEvent*\0startTimer\0"
+    "stopTimer\0timeout\0resetTimer\0wrongPin\0"
+    "on_pushButton_Ok_clicked\0"
+    "on_pushButton_Clear_clicked\0"
+    "on_pushButton_0_clicked\0on_pushButton_1_clicked\0"
+    "on_pushButton_2_clicked\0on_pushButton_3_clicked\0"
+    "on_pushButton_4_clicked\0on_pushButton_5_clicked\0"
+    "on_pushButton_6_clicked\0on_pushButton_7_clicked\0"
+    "on_pushButton_8_clicked\0on_pushButton_9_clicked"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_LoginUi[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      20,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       2,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags
+       1,    1,  114,    2, 0x06 /* Public */,
+       3,    0,  117,    2, 0x06 /* Public */,
+
+ // slots: name, argc, parameters, tag, flags
+       4,    1,  118,    2, 0x08 /* Private */,
+       6,    0,  121,    2, 0x08 /* Private */,
+       7,    0,  122,    2, 0x08 /* Private */,
+       8,    0,  123,    2, 0x08 /* Private */,
+       9,    0,  124,    2, 0x08 /* Private */,
+      10,    1,  125,    2, 0x08 /* Private */,
+      11,    0,  128,    2, 0x08 /* Private */,
+      12,    0,  129,    2, 0x08 /* Private */,
+      13,    0,  130,    2, 0x08 /* Private */,
+      14,    0,  131,    2, 0x08 /* Private */,
+      15,    0,  132,    2, 0x08 /* Private */,
+      16,    0,  133,    2, 0x08 /* Private */,
+      17,    0,  134,    2, 0x08 /* Private */,
+      18,    0,  135,    2, 0x08 /* Private */,
+      19,    0,  136,    2, 0x08 /* Private */,
+      20,    0,  137,    2, 0x08 /* Private */,
+      21,    0,  138,    2, 0x08 /* Private */,
+      22,    0,  139,    2, 0x08 /* Private */,
+
+ // signals: parameters
+    QMetaType::Void, QMetaType::QString,    2,
+    QMetaType::Void,
+
+ // slots: parameters
+    QMetaType::Void, 0x80000000 | 5,    2,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QString,    2,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void LoginUi::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<LoginUi *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->sendPinToEngine((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        case 1: _t->aboutToQuit(); break;
+        case 2: _t->closeEvent((*reinterpret_cast< QCloseEvent*(*)>(_a[1]))); break;
+        case 3: _t->startTimer(); break;
+        case 4: _t->stopTimer(); break;
+        case 5: _t->timeout(); break;
+        case 6: _t->resetTimer(); break;
+        case 7: _t->wrongPin((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        case 8: _t->on_pushButton_Ok_clicked(); break;
+        case 9: _t->on_pushButton_Clear_clicked(); break;
+        case 10: _t->on_pushButton_0_clicked(); break;
+        case 11: _t->on_pushButton_1_clicked(); break;
+        case 12: _t->on_pushButton_2_clicked(); break;
+        case 13: _t->on_pushButton_3_clicked(); break;
+        case 14: _t->on_pushButton_4_clicked(); break;
+        case 15: _t->on_pushButton_5_clicked(); break;
+        case 16: _t->on_pushButton_6_clicked(); break;
+        case 17: _t->on_pushButton_7_clicked(); break;
+        case 18: _t->on_pushButton_8_clicked(); break;
+        case 19: _t->on_pushButton_9_clicked(); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        {
+            using _t = void (LoginUi::*)(QString );
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&LoginUi::sendPinToEngine)) {
+                *result = 0;
+                return;
+            }
+        }
+        {
+            using _t = void (LoginUi::*)();
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&LoginUi::aboutToQuit)) {
+                *result = 1;
+                return;
+            }
+        }
+    }
+}
+
+QT_INIT_METAOBJECT const QMetaObject LoginUi::staticMetaObject = { {
+    QMetaObject::SuperData::link<QDialog::staticMetaObject>(),
+    qt_meta_stringdata_LoginUi.data,
+    qt_meta_data_LoginUi,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *LoginUi::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *LoginUi::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_LoginUi.stringdata0))
+        return static_cast<void*>(this);
+    return QDialog::qt_metacast(_clname);
+}
+
+int LoginUi::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QDialog::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 20)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 20;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 20)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 20;
+    }
+    return _id;
+}
+
+// SIGNAL 0
+void LoginUi::sendPinToEngine(QString _t1)
+{
+    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(std::addressof(_t1))) };
+    QMetaObject::activate(this, &staticMetaObject, 0, _a);
+}
+
+// SIGNAL 1
+void LoginUi::aboutToQuit()
+{
+    QMetaObject::activate(this, &staticMetaObject, 1, nullptr);
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
